42d4f0fa9100e10ff110f8741a2bc8870ff386ba
Pedro1Gustavo <pedroguga2003@gmail.com>
2024-11-18 16:53:32 -0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+Prova 1
+
 for numero in range(1,30):
   if numero % 3 == 0:
-    print(numero)
+    print(numero) 
+
+Prova 2 
+
+numero = int(input("Digite um número para ver a tabuada: "))
+contador = 1
+while contador <= 10:
+    resultado = numero * contador
+    print(f"{numero} x {contador} = {resultado}")
+    contador += 1 
